db5925a342601687df6c7f85278cacd97c35cdc5
Danish Alam <dredwing05@gmail.com>
2024-10-28 22:04:31 +0530

    removed cors as not needed

--- index.js
+++ index.js
@@ -1,45 +1,35 @@
 import express from "express";
 import cookieParser from "cookie-parser";
-import cors from "cors";
 import dotenv from "dotenv";
 import connectDB from "./utils/db.js";
 import userRoute from "./routes/user.route.js";
 import companyRoute from "./routes/company.route.js";
 import jobRoute from "./routes/job.route.js";
 import applicationRoute from "./routes/application.route.js";
-
-dotenv.config({});
 import swaggerUi from 'swagger-ui-express';
-// import swaggerDocument from './swagger-output.json';
 import swaggerDocument from './swagger-output.json' assert { type: 'json' };
 
+dotenv.config();
 
 const app = express();
+
+// Swagger setup
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
-// middleware
+
+// Middleware
 app.use(express.json());
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
-const corsOptions = {
-    // origin:'http://localhost:5173',
-    origin:'*',
-    credentials:true
-}
 
-app.use(cors(corsOptions));
-
-const PORT = process.env.PORT || 3000;
-
-
-// api's
+// API Routes
 app.use("/api/v1/user", userRoute);
 app.use("/api/v1/company", companyRoute);
 app.use("/api/v1/job", jobRoute);
 app.use("/api/v1/application", applicationRoute);
 
-
-
-app.listen(PORT,()=>{
-    connectDB();
-    console.log(`Server running at port http://localhost:${PORT}`);
-})
+// Start the server and connect to the database
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, async () => {
+    await connectDB();
+    console.log(`Server running at http://localhost:${PORT}`);
+});
