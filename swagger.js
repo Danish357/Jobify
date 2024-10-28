b889df6734756dda7e3345286524374eaf83b965
Danish Alam <dredwing05@gmail.com>
2024-10-28 20:12:30 +0530

    Update swagger configuration in swagger.js

--- swagger.js
+++ swagger.js
@@ -1,13 +1,13 @@
 import swaggerAutogen from 'swagger-autogen';
-
+const isProduction = process.env.NODE_ENV === 'production';
 const doc = {
   info: {
     title: 'My API',
-    description: 'Description'
+    description: 'This is a detailed description of the API endpoints and their usage.'
   },
-  host: 'localhost:5000',
-//   basePath: '/api/v1', // <-- Set the correct base path here
-  schemes: ['http'],
+  host: isProduction ? 'jobify-q4yg.onrender.com' : 'localhost:5000',
+   basePath: '/api/v1', // <-- Set the correct base path here
+   schemes: [isProduction ? 'https' : 'http'],
 };
 
 const outputFile = './swagger-output.json';
